e9832b8d0e1d9a6fe6d6ef92eafd34f59cc3a1e5
a-dri-an-S <adrian.salinas.1986@gmail.com>
2021-04-24 17:25:51 -0700

    added Pagination component + created links for dynamic page rendering

--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -28,6 +28,7 @@ export default function DeleteProduct({ id, children }) {
       type="button"
       disables={loading}
       onClick={() => {
+        // eslint-disable-next-line no-restricted-globals
         if (confirm('Are you sure you want to delete this item?!')) {
           // go ahead and delete it
           console.log('Delete');
