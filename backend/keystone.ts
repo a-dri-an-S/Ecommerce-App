1ad56e4033afb1d44fc0f2ed2eea70c76f8931ba
a-dri-an-S <adrian.salinas.1986@gmail.com>
2021-04-29 20:55:11 -0700

    created custom mutation index.ts + addToCarts.ts to add items to cart

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -13,6 +13,7 @@ import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
 const databaseURL =
   process.env.DATABASE_URL || 'monoDB://localhost/keystone-sick-fits-tutorial';
@@ -63,6 +64,7 @@ export default withAuth(
       ProductImage,
       CartItem,
     }),
+    extendGraphqlSchema,
     ui: {
       // Show the UI only for people who pass this test
       isAccessAllowed: ({ session }) => !!session?.data,
