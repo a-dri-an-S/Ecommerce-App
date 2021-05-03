ad247a610b5d0ac86dd2f9bda54943f25780bdb8
a-dri-an-S <adrian.salinas.1986@gmail.com>
2021-05-02 17:55:55 -0700

    created permissions access functions, access.ts

--- backend/access.ts
+++ backend/access.ts
@@ -1,7 +1,24 @@
 // At its simplest, the access control returns a yes or no value depending on the users session
 
 import { ListAccessArgs } from './types';
+import { permissionsList } from './schemas/fields';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissions check if someone meets a criteria - yes or no.
+export const permissions = {
+  ...generatedPermissions,
+};
+
+// Rule based functions
